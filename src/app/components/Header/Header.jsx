163a8c3423bd23ce70b24da1d8a9866b38f601f4
zuritajonny <jonnyzuritadev@gmail.com>
2023-09-28 15:41:49 -0400

    Optimizations

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import Logo from "../../assets/images/logo.png";
 function Header() {
   return (
-    <header className={`${styles.header_container} max-gl-wdh`}>
+    <header className={`${styles.header_container} `}>
       <div
-        className={`${styles.header_wrapper} v-align-center h-justify-center twoCol`}
+        className={`${styles.header_wrapper} max-gl-wdh v-align-center h-justify-center twoCol`}
       >
         <Image
           priority
