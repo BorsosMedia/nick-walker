dacea9095bdd126ec01b94caab0e9570a5701224
Borsos Media <137100938+BorsosMedia@users.noreply.github.com>
2023-10-02 14:00:19 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,16 +27,7 @@ const ptsans = PT_Sans_Narrow({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      // <Script strategy="lazyOnload">
-      //   {`
-      //               window.dataLayer = window.dataLayer || [];
-      //               function gtag(){dataLayer.push(arguments);}
-      //               gtag('js', new Date());
-      //               gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-      //               page_path: window.location.pathname,
-      //               });
-      //           `}
-      // </Script>
+
       <head>
         <script async src=`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`></script>
         <script src={gtm}></script>
