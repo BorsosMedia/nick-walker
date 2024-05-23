064ce7d142fda2b0614c53a15a55a7c636f8e56c
zuritajonny <jonnyzuritadev@gmail.com>
2024-05-23 14:25:47 -0400

    Widget Lazy Load

--- src/app/components/YoutubeWidget.jsx
+++ src/app/components/YoutubeWidget.jsx
@@ -3,7 +3,11 @@ import { ElfsightWidget } from "react-elfsight-widget";
 function YoutubeWidget() {
   return (
     <section className="block-content max-gl-wdh">
-      <ElfsightWidget widgetID="d994e31d-b7ff-455a-8958-1df16cca793c" />
+      <ElfsightWidget
+        widgetID="d994e31d-b7ff-455a-8958-1df16cca793c"
+        data-elfsight-app-lazy
+        lazy
+      />
     </section>
   );
 }
